d3f00ae1b978c4110bfb3cbc2465048933309626
Hugo TRINH <hugo.trinh-quy@epitech.eu>
2019-03-07 09:42:41 +0100

    correct strcasecmp

--- src/strcasecmp.asm
+++ src/strcasecmp.asm
@@ -8,45 +8,44 @@ strcasecmp:
     push    rcx
     mov     rbp, rsp
     xor     rcx, rcx
-    mov     r8, rdi
-    mov     r9, rsi
-    jmp     loop_1
 
 loop_1:
-    cmp     BYTE [r8 + rcx], 0
+    movzx   r8, BYTE [rdi + rcx]
+    movzx   r9, BYTE [rsi + rcx]
+    cmp     r8b, 0
     je      end
-    cmp     BYTE [r9 + rcx], 0
+    cmp     r9b, 0
     je      end
 
 compare_upper_first:
-    cmp     BYTE [r8 + rcx], 'A'
+    cmp     r8b, 'A'
     jae     compare_upper_z
-    jmp     loop_2
+    jmp     compare_upper_sec
 
 compare_upper_z:
-    cmp     BYTE [r8 + rcx], 'Z'
+    cmp     r8b, 'Z'
     jbe     to_lower_first
-    jmp     loop_2
+    jmp     compare_upper_sec
 
 to_lower_first:
-    xor     BYTE [r8 + rcx], 32
+    xor     r8b, 32
 
 compare_upper_sec:
-    cmp     BYTE [r9 + rcx], 'A'
+    cmp     r9b, 'A'
     jae     compare_z_sec
     jmp     loop_2
 
 compare_z_sec:
-    cmp     BYTE [r9 + rcx], 'Z'
+    cmp     r9b, 'Z'
     jbe     to_lower_sec
     jmp     loop_2
 
 to_lower_sec:
-    xor     BYTE [r9 + rcx], 32
+    xor     r9b, 32
 
 loop_2:
-    mov     dl, [r9 + rcx]
-    cmp     BYTE [r8 + rcx], dl
+    mov     dl, r9b
+    cmp     r8b, dl
     jne     end
     inc     rcx
     jmp     loop_1
