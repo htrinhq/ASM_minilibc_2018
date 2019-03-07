8fc186858d863b13f3c4aaa13e6d4d3edc3dfb3e
Hugo TRINH <hugo.trinh-quy@epitech.eu>
2019-03-07 12:06:49 +0100

    add rindex

--- src/rindex.asm
+++ src/rindex.asm
@@ -10,8 +10,26 @@ rindex:
     mov     rbp, rsp
     call    strlen wrt ..plt
     mov     rcx, rax
+    dec     rcx
+
+loop:
+    cmp     BYTE [rdi + rcx], sil
+    je      get_char
+    cmp     rcx, 0
+    je      no_char
+    dec     rcx
+    jmp     loop
+
+get_char:
+    add     rdi, rcx
+    mov     rax, rdi
+    jmp     end
+
+no_char:
+    mov     rax, 0
 
 end:
     mov     rsp, rbp
     pop     rcx
     pop     rbp
+    ret
--- src/strchr.asm
+++ src/strchr.asm
@@ -8,21 +8,21 @@ strchr:
     mov     rbp, rsp
 
 loop:
-    cmp BYTE [rdi], sil
-    je  get_char
-    cmp BYTE [rdi], 0
-    je  no_char
-    inc rdi
-    jmp loop
+    cmp     BYTE [rdi], sil
+    je      get_char
+    cmp     BYTE [rdi], 0
+    je      no_char
+    inc     rdi
+    jmp     loop
 
 get_char:
-    mov rax, rdi
-    jmp end
+    mov     rax, rdi
+    jmp     end
 
 no_char:
-    mov rax, 0
+    mov     rax, 0
 
 end:
-    mov rsp, rbp
-    pop rbp
+    mov     rsp, rbp
+    pop     rbp
     ret
